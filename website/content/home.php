0539ea47a2d42d23119cd7893db4b867f0a034a6
Ganesh V <83175090+vganeshmanian@users.noreply.github.com>
2021-12-23 10:26:36 +0530

    Update home.php

--- website/content/home.php
+++ website/content/home.php
@@ -1,2 +1,4 @@
 <p>This is <b>home</b> Welcome to test project -- this content is in file -- content/home.php
 Feel free to edit it and check in git to test the CI/CD flow</p>
+
+<h2> Completed DevOps Project 1</h1>
